1e3466fb05f32608f0006a298a9b85e4429d801e
Afadinsro <mills.josef@gmail.com>
2017-12-17 00:23:16 +0000

    Member and Bacenta classes created

---
+++ app/src/main/java/com/adino/mta/Member.java
@@ -0,0 +1,56 @@
+package com.adino.mta;
+
+/**
+ * Created by afadinsro on 12/17/17.
+ */
+
+public class Member {
+
+    private String name;
+    private String email;
+    private String calling_number;
+    private String whatsapp_number;
+    private String location;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCalling_number() {
+        return calling_number;
+    }
+
+    public void setCalling_number(String calling_number) {
+        this.calling_number = calling_number;
+    }
+
+    public String getWhatsapp_number() {
+        return whatsapp_number;
+    }
+
+    public void setWhatsapp_number(String whatsapp_number) {
+        this.whatsapp_number = whatsapp_number;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ app/src/main/java/com/adino/mta/bacenta/UniBacenta.java
@@ -0,0 +1,8 @@
+package com.adino.mta.bacenta;
+
+/**
+ * Created by afadinsro on 12/17/17.
+ */
+
+public class UniBacenta {
+}
